a8e6c89ccd32b2ab09c176d768ac9207e2832ed2
Zachary F Horvitz <zfh2000@mlogin001.eth.cluster>
2023-12-14 19:12:24 -0500

    crunch time

--- scripts/explore_latents.py
+++ scripts/explore_latents.py
@@ -2,19 +2,54 @@ import pyro
 import torch
 import json
 import pickle
-
+import os 
 from prodslda_cls import ProdSLDA
 
-MODEL_PATH = '/burg/nlp/users/zfh2000/style_results/pos_bigrams/2023-12-14_17_54_45/model_epoch5_20914.218841552734.pt'
-DATA_PATG = '/burg/nlp/users/zfh2000/style_results/pos_bigrams/maxdf0.5_mindf5_DATA'
-pyro.clear_param_store()
+from sklearn.feature_extraction.text import CountVectorizer
 
-prodsdla = torch.load('prod_slda_saved_model')
-prodsdla.eval()
+MODEL_PATH = '/burg/nlp/users/zfh2000/style_results/pos_bigrams/2023-12-14_17_54_45/model_epoch5_20914.218841552734.pt'
+DATA_DIR_PATH = '/burg/nlp/users/zfh2000/style_results/pos_bigrams/maxdf0.5_mindf5_DATA'
+
+with open(os.path.join(DATA_DIR_PATH, 'bows.pickle'), 'rb') as in_file:
+    bows = pickle.load(in_file)
+        
+with open(os.path.join(DATA_DIR_PATH, 'meta_vectorized.pickle'), 'rb') as in_file:
+    meta_vectorized = pickle.load(in_file)    
+
+with open(os.path.join(DATA_DIR_PATH, "raw_text.json"), 'r') as in_file:
+    raw_text = json.load(in_file)    
+
+with open(os.path.join(DATA_DIR_PATH, "authors_json.json"), 'r') as in_file:
+    authors_json = json.load(in_file)    
+
+with open(os.path.join(DATA_DIR_PATH, "meta_feature_to_names.json"), 'r') as in_file:
+    meta_feature_to_names = json.load(in_file)
+
+with open(os.path.join(DATA_DIR_PATH, "vectorizer.pickle"), 'rb') as in_file:
+    vectorizer = pickle.load(in_file)
+
+
 import pdb; pdb.set_trace()
 
+pyro.clear_param_store()
 
-# with torch.no_grad():
+prodsdla = torch.load(MODEL_PATH)
+prodsdla.eval()
+
+
+with torch.no_grad():
+
+    for text, author, bow, meta in zip(raw_text['training'], authors_json['training'], bows['training'], meta_vectorized['training']):
+        print(author, text)
+        # print(text)
+        # print(author)
+        # print(bow)
+        # print(meta)
+        # print('------------------')
+        # result =  F.softmax(prodsdla.guide(bow.unsqueeze(0), meta.unsqueeze(0))[1])
+        # print(result)
+        # print('------------------')
+        # print('------------------'
 
 
 # label_to_topic = {}
